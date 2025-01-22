b5a31e26c03b4253c5ff31f37e9f7815f381cd68
biFebriansyah <bukanebi@gmail.com>
2025-01-22 20:52:12 +0700

    new: change database source

--- src/utils/mongo.ts
+++ src/utils/mongo.ts
@@ -6,16 +6,14 @@ export class MongoModule {
   static forRoot(): DynamicModule {
     return {
       imports: [
-        MongooseModule.forRoot(
-          `mongodb://${process.env.MONGODB_HOST}/develop`,
-          {
-            user: process.env.MONGODB_USER,
-            pass: process.env.MONGODB_PASS,
-            authSource: 'admin',
-            replicaSet: 'mongors',
-            directConnection: true,
-          },
-        ),
+        MongooseModule.forRoot(`mongodb://${process.env.MONGODB_HOST}`, {
+          user: process.env.MONGODB_USER,
+          pass: process.env.MONGODB_PASS,
+          dbName: process.env.MONGODB_DB,
+          authSource: 'admin',
+          replicaSet: 'mongors',
+          directConnection: true,
+        }),
       ],
       module: MongoModule,
     };
